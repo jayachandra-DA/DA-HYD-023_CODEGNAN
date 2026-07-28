cddf87245c0ae834c07794d23fa97dcf68f953d6
jayachandra-DA <girijayachandra117gmail.com>
2026-07-28 15:39:52 +0530

    Day1

---
+++ Day1.py
@@ -0,0 +1,9 @@
+print("Hello Codegnan")
+'''tokens are of 5 tyes'''
+name = 'jaya chandra'
+print(name)
+email_id = 'girijayachandra117@gmail.com'
+print(email_id)
+a5ail='hsfth'
+name,age,location='jaya',22,'hyd'
+print(name,age,location)
