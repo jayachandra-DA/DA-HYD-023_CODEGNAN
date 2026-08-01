2ee2c0e3789615b1876bbc46daa2a0d439c026d5
jayachandra-DA <girijayachandra117gmail.com>
2026-08-01 14:12:44 +0530

    'outputs'

--- Day4.py
+++ Day4.py
@@ -1,4 +1,4 @@
-
+'''
 a=45
 b=45
 print(id(a))
@@ -8,8 +8,7 @@ print(a is b)
 
 a=[50,60,70,80]
 b=[50,60,70,80]
-print(id(a))  # the list is mutable (in the list we can add or delete the numbers so the storage address
-is different  )
+print(id(a))  # the list is mutable (in the list we can add or delete the numbers so the storage addressis different  )
 print(id(b))
 print(a is b)
 
@@ -33,17 +32,20 @@ print(5<<1)             #   16 8 4 2 1
 print(5>>1)
 
 
-
+'''
 #conditional block statements
 #1) conditional statements ------> if ,else and elif
 #2) repetional statements ------> for and while
 age=int(input("Enter your age: "))
-if age<=0:
-    print('enter a valuble age')
-    if age > 18:
+if age>0:
+    if age < 18:
         print(f"Your Age is :{age}. So, You are not Eligible to Vote")
     else:
-        print('you are not Eligible')
+        print('you are Eligible')
+else:
+    print('enter a valid age')
+
+
 
 
 
