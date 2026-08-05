ab2f32adacfedc58d43993f692595f505e61002c
jayachandra-DA <girijayachandra117gmail.com>
2026-08-05 13:19:15 +0530

    'strings'

---
+++ Day8.py
@@ -0,0 +1,70 @@
+
+#sequence-----> strings,lists,tuples,mapping()and sets
+name='jaya chandra'
+print(name)
+print(type(name))
+print(len(name))
+#index------->fetch the object(position) start at 0 ends at len(obj)-1
+print(name[0])
+print(name[3])
+#print(name[25])  #index error out of range
+print(name[-1])
+print(name[-3])
+print(name[0:3])
+print(name[:])
+print(name[:])
+print(name[:25])# it prints full sentance
+print(name[:-1])#
+#strding------>step
+name='jaya chandra'
+print(name[::1])
+print(name[::2])
+print(name[1:10:2])
+c='dataAnalysis'
+print(name[::-1])
+#concatenantion, repetation, indexing
+
+name= 'jaya'+'chandra'
+print(name)
+print(name*5)
+#built-in punctions
+c='jaya'
+print(len(c))
+print(min(c))
+print(sorted(c))
+print(max(c))
+#case convertion------>upper,lower,title and capitalized
+print(c.upper())
+print(c.lower())
+print(c.title())
+print(c.capitalize())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
