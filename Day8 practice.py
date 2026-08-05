3244af1f0a0f633a79dc093244a986851217fbab
jayachandra-DA <girijayachandra117gmail.com>
2026-08-05 22:53:00 +0530

    'day8_practice'

---
+++ Day8 practice.py
@@ -0,0 +1,25 @@
+name = "Jaya Chandra"
+
+print(name[0])      
+print(name[0:4])    
+print(name[5:])   
+print(name[:4])      
+print(name[-1])    
+print(name[-7:])   
+print(name[::2])     
+print(name[1::2])    
+print(name[::-1])    
+print(name[2:9])     
+print(name[::3])     
+print(name[-5:-1])
+
+
+#weite a program that prints (A-Z) alphabets
+for i in range(65,91):
+    print(chr(i),end=", ")
+
+i=65
+while i<91:
+    print(chr(i),end=", ")
+    i+=1
+    
